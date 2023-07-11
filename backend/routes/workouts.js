ba4fd1816af357d6c4edb515423984437c6f2f45
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2023-07-11 05:49:03 -0700

    Creating a workout model(class) and creating a post request

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Workout = require("../models/workoutModel");
 
 // this will bring up an instance of the router class
 const router = express.Router()
@@ -11,25 +12,35 @@ router.get("/", (req, res)=>{
 
 // the : will reill add a parameter
 router.get("/:id", (req, res)=>{
-    res.json({mssg:"GET single workouts"})
+    res.json({mssg:"GET single workouts"});
 
 })
 
 
-router.post("/", (req, res)=>{
-    res.json({mssg:"POST single workouts"})
+router.post("/", async (req, res)=>{
+    const {title, load, reps} = req.body; 
+
+    try{
+        const workout = await Workout.create({title,load, reps})
+        res.status(200).json(workout);
+
+    } catch (error){
+        // the error has a messag on the class
+        res.status(400).json({error: error.message})
+    }
+
 
 })
 
 
 router.delete("/:id", (req, res)=>{
-    res.json({mssg:"Delete a workout"})
+    res.json({mssg:"Delete a workout"});
 
 })
 
 
 router.patch("/:id", (req, res)=>{
-    res.json({mssg:"Update a workout"})
+    res.json({mssg:"Update a workout"});
 
 })
 
